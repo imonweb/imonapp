e88060c718a5e0eec72b68a9833e9f59f36eec45
Imon <contact@imonweb.co.uk>
2022-06-06 16:31:59 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+         <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </BrowserRouter>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      
     </div>
   );
+}
+
+function HomePage(){
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+function DashboardPage(){
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  )
 }
 
 export default App;
